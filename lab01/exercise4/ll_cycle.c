aef282b0a084c3c36ddc9b14aaeb9c2e023c0bcb
chzhang <chzhang@ubuntu.com>
2021-10-16 21:38:25 -0700

    Finish lab1

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    int num_cycle = 0;
+    node * fast_ptr;
+    node * slow_ptr;
+    fast_ptr = head;
+    slow_ptr = head;
+    while(1)
+    {
+        if(fast_ptr == NULL) return num_cycle;
+        fast_ptr = fast_ptr -> next;
+        if(fast_ptr == NULL) return num_cycle;
+        fast_ptr = fast_ptr -> next;
+        if(fast_ptr == NULL) return num_cycle;
+
+        slow_ptr = slow_ptr -> next;
+        if(fast_ptr == slow_ptr) return ++num_cycle;
+    }
+
 }
