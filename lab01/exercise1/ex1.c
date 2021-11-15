aba35b104ab1764d010dff23e19d612a54ded76e
chzhang <chzhang@ubuntu.com>
2021-11-14 23:05:49 -0800

    Fix some error in lab1

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -1,6 +1,6 @@
 #include <string.h>
 #include "ex1.h"
-
+#include <stdio.h>
 /* Returns the number of times LETTER appears in STR.
 There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
@@ -15,6 +15,7 @@ int num_occurrences(char *str, char letter) {
         {
             num++;
         }
+        i++;
     }
     return num;
 }
@@ -24,5 +25,9 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    dna_seq->A_count = num_occurrences(dna_seq->sequence, 'A');
+    dna_seq->C_count = num_occurrences(dna_seq->sequence, 'C');
+    dna_seq->G_count = num_occurrences(dna_seq->sequence, 'G');
+    dna_seq->T_count = num_occurrences(dna_seq->sequence, 'T');
     return;
 }
